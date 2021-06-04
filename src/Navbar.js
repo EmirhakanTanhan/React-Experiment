da6180e50bee9a81aa1588c532196f3d5773949b
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-06-04 03:14:18 +0300

    imp

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <h1>Experiment Blog</h1>
+                <div className="links">
+                    <Link to="/">Home</Link>
+                    <Link to="/create">New Blog</Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
