49b086646c94a023679079aacac8c59fe42a2218
Emirhakan Tanhan <emirhakan.tanhan@gmail.com>
2021-06-05 03:54:57 +0300

    imp

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,32 @@
-import { useParams } from 'react-router-dom';
+import {useHistory, useParams} from 'react-router-dom';
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const {data, isPending, error} =useFetch('http://localhost:8000/blogs/'+id);
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/'+id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return (
         <div className="blog-details">
-            <h2>Blog Details {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {data && (
+                <article>
+                    <h2>{data.title}</h2>
+                    <p>Written by {data.author}</p>
+                    <div>{data.body}</div>
+                    <button onClick={handleClick}>delete</button>
+                </article>
+            )}
+            <p></p>
         </div>
     );
 };
